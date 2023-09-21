fab51c3e37894bb281106c83e05df4c0ec958c80
kellworks <kchidothi@outlook.com>
2023-09-21 17:57:56 +0200

    build main.go

--- api/main.go
+++ api/main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-	"encoding/json",
-	"log",
-	"net/http",
-	"strings",
-	"time",
-	"context",
-	"os",
-	"os/signal",
-	"errors",
-	"github.com/go-chi/chi",
-	"github.com/go-chi/chi/middleware",
-	"github.com/thedevsaddam/renderer",
-	mgo "gopkg.in/mgo.v2",
-	"gopkg.in/mgo.v2/bson"
+	"encoding/json";
+	"log";
+	"net/http";
+	"strings";
+	"time";
+	"context";
+	"os";
+	"os/signal";
+	"github.com/go-chi/chi";
+	"github.com/go-chi/chi/middleware";
+	"github.com/thedevsaddam/renderer";
+	mgo "gopkg.in/mgo.v2";
+	"gopkg.in/mgo.v2/bson";
 )
 
-var render *renderer.Render
-val db *mgo.Database
+var rnd *renderer.Render
+var db *mgo.Database
 
 const(
-	hostname string = "localhost:2413",
-	dbName string = "todoDB",
-	collectionName string = "todo",
+	hostName string = "localhost:2413"
+	dbName string = "todoDB"
+	collectionName string = "todo"
 	port string = ":9090"
 )
 
@@ -36,7 +35,7 @@ type(
 		CreatedAt time.Time `bson: "created_at"`
 	}
 	todo struct {
-		ID bson.ObjectId `json: "_id,omitempty"`
+		ID string `json: "_id,omitempty"`
 		Title string `json: "title"`
 		Notes string `json: "notes"`
 		Completed bool `json: "completed"`
@@ -50,7 +49,7 @@ func createTodo(w http.ResponseWriter, r *http.Request){
 		rnd.JSON(w, http.StatusProcessing, err)
 		return
 	}
-	if t.title  == ""{
+	if t.Title  == ""{
 		rnd.JSON(w, http.StatusBadRequest, renderer.M{
 			"message": "title is required",
 		})
@@ -58,29 +57,29 @@ func createTodo(w http.ResponseWriter, r *http.Request){
 	}
 	tm := todoModel{
 		ID: bson.NewObjectId(),
-		Title: t.title,
-		Notes: t.notes,
+		Title: t.Title,
+		Notes: t.Notes,
 		Completed: false,
-		CreatedAt: time.Now()
+		CreatedAt: time.Now(),
 	}
-	if err := db.C(collectionName).insert(tm); err != nil {
-		rnd.JSON(w, status.StatusProcessing, rendered.M{
+	if err := db.C(collectionName).Insert(&tm); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
 			"message": "Could not save todo",
-			"error": err
+			"error": err,
 		})
 	}
 
-	rnd.JSON(w, status.StatusCreated, rendered.M{
-		"message": "Todo created successfully"
+	rnd.JSON(w, http.StatusCreated, renderer.M{
+		"message": "Todo created successfully",
 	})
 }
 
 func getTodos(w http.ResponseWriter, r *http.Request){
 	todos := []todoModel{}
 	if err := db.C(collectionName).Find(bson.M{}).All(&todos); err != nil {
-		rnd.JSON(w http.StatusProcessing, renderer.M{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
 			"message": "Could not get todos",
-			"error": err
+			"error": err,
 		})
 		return
 	}
@@ -89,42 +88,42 @@ func getTodos(w http.ResponseWriter, r *http.Request){
 		todoList = append(todoList, todo{
 			ID: t.ID.Hex(),
 			Title: t.Title,
-			Note: t.Note,
+			Notes: t.Notes,
 			Completed: t.Completed,
-			CreatedAt: t.CreatedAt
+			CreatedAt: t.CreatedAt,
 		})
 	}
-	rnd.JSON(w http.StatusOK, renderer.M{
-		"data": todoList
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"data": todoList,
 	})
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request){
 	id := strings.TrimSpace(chi.URLParam(r, "id"))
-	if !bson.isObjectHex(id){
-		rnd.JSON(w, status.StatusBadRequest, renderer.M{
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
 			"message": "Todo not found",
 		})
 		return
 	}
 
-	if err := db.C(collectionName).RemoveId(bson.objectIdHex(id)); err != nil {
-		rnd.JSON(w, status.StatusProcessing, renderer.M{
+	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
 			"message": "Failed to remove todo",
-			"error": err
+			"error": err,
 		})
 		return
 	}
 
-	rnd.JSON(w, status.StatusOK, renderer.M{
+	rnd.JSON(w, http.StatusOK, renderer.M{
 		"message": "Todo deleted successfully",
 	})
 }
 
 func updateTodo(w http.ResponseWriter, r *http.Request){
 	id := strings.TrimSpace(chi.URLParam(r, "id"))
-	if !bson.isObjectHex(id){
-		rnd.JSON(w, status.StatusBadRequest, renderer.M{
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
 			"message": "Todo not found",
 		})
 		return
@@ -132,27 +131,26 @@ func updateTodo(w http.ResponseWriter, r *http.Request){
 
 	var t todo
 	if err := json.NewDecoder(r.Body).Decode(&t); err != nil{
-		rnd.JSON(w, status.StatusProcessing, renderer.M{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
 			"message": "Todo not found",
 		})
 		return
 	}
 
-	if t.title != ""{
-		rnd.JSON(w, status.StatusProcessing, renderer.M{
+	if t.Title != ""{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
 			"message": "Title is required",
 		})
 		return
 	}
 
-	if err := db.C(collectionName)
-	.update(
+	if err := db.C(collectionName).Update(
 		bson.M{"_id": bson.ObjectIdHex(id)},
-		bson.M{"title": t.title, "completed": t.completed, "notes": t.notes}
-	); err != nil {
-		rnd.JSON(w, status.StatusProcessing, renderer.M{
+		bson.M{"title": t.Title, "completed": t.Completed, "notes": t.Notes});
+		err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
 			"message": "Failed to update todo",
-			"error": err
+			"error": err,
 		})
 		return
 	}
@@ -160,9 +158,9 @@ func updateTodo(w http.ResponseWriter, r *http.Request){
 
 func init(){
 	rnd = renderer.New()
-	sess,err:=mgo.Dial(hostname)
+	sess, err := mgo.Dial(hostName)
 	checkError(err)
-	sess.setSession(mgo.Monotonic, true)
+	sess.SetMode(mgo.Monotonic, true)
 	db = sess.DB(dbName)
 }
 
@@ -171,7 +169,7 @@ func main(){
 	signal.Notify(sChannel, os.Interrupt)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", homeHandler())
+	r.Get("/", homeHandler)
 	r.Mount("/todo", todoHandler())
 
 	srv := &http.Server{
@@ -191,13 +189,12 @@ func main(){
 	log.Println("Shutting down server")
 	ctx, cancel := context.WithTimeout(context.Background(),5*time.Second)
 	srv.Shutdown(ctx)
-	defer.canel(
-		log.Println("Gracefully shutting down server")
-	)
+	defer cancel()
+	log.Println("Gracefully shutting down server")
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request){
-	err := rnd.template(w, http.StatusOK, []string("/static/home.tpl"))
+	err := rnd.Template(w, http.StatusOK, []string{"/static/home.tpl"}, nil)
 	checkError(err)
 }
 
@@ -214,7 +211,6 @@ func todoHandler() http.Handler {
 
 func checkError(err error){
 	if err != nil {
-		log.fatal(err)
-		return nil, err
+		log.Fatal(err)
 	}
 }
